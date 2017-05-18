202e9e81c5f3c80dd74d3d7ec12274d17e9e0d3d
Joao Matos Silva <joao@kspace.pt>
2017-05-18 20:43:34 +0100

    Added Original and Feature Folders Samples

---
+++ MvcMusicStore-v3.0-FeatureFolders/MvcMusicStore/Features/ShoppingCart/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Features.ShoppingCart
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
---
+++ MvcMusicStore-v3.0-FeatureFolders/MvcMusicStore/Infrastructure/FeatureFolders/ControllerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMusicStore.Infrastructure.FeatureFolders
+{
+    public class ControllerFactory : DefaultControllerFactory
+    {
+        protected override Type GetControllerType(RequestContext requestContext, string controllerName)
+        {
+            return
+                typeof(ControllerFactory).Assembly.GetType("MvcMusicStore.Features." + controllerName +
+                                                            "." + controllerName + "Controller");
+        }
+    }
+}
